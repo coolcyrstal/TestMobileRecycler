ae96edb984ab23d16d022be51557f4bd022ea654
coolcyrstal <chayenjr.sk131@gmail.com>
2017-03-27 18:01:53 +0700

    init

---
+++ app/src/main/java/com/example/chayen/testmobilerecycler/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chayen.testmobilerecycler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
